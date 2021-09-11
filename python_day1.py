0a42797af6599d4d449c19417ab65f62052bb441
rahulambhore3 <rahulambhore3@gmail.com>
2021-09-11 18:51:36 +0530

    adding day1 and ay2 work

--- python_day1.py
+++ python_day1.py
@@ -1,41 +1,21 @@
+def myfirstfun(normal, *args, **kwargs):
+    print(normal)
+    for i in args:
+        print(i)
+    for key, value in kwargs.items():
+        print(f"{key} is a {value}")
+normal = 'this is so good'
+har = ['test', 'waste', 'toot']
+tar = {"Rahul": "Eng", "Pankaj":"IO"}
+myfirstfun(normal, *har, **tar)
+
 #Task1
-test = 'this is test'
-print(len(test))  #printing lenth of test variable
-print(test.upper()) #converting test variable to upper case
-print(type(test)) # checking type of test variable
-number = 25
-print(number)
-print(type(number)) # printing type of number
+def day2(normal, *args, **kwargs):
+    print(normal)
+    for i in args:
+        print(i + i)
 
-test = [1,2,3,4]
-print(test)
-test.append(5)
-print(test)
-print(type(test))
-
-#Task2:
-
-test = [1,2,3,4,5] # we need to provide value to test and it will print accoundigly
-for i in test:
-    print(i)
-
-i = 0
-for i in range(11): # this way we can just range it and no need to give long value
-    print(i)
-
-table = int(input('please enter the table you want to print? ')) # you can take any user input to print any table
-for i in range(1,11):
-    print(table, 'x', i, '=', table*i)
-    #print(table*i) # this will also do 
-    
-#Task3
-number = [2,3,5,8,9]
-for i in number:
-    if i%2 ==0:
-        print('printing even number')
-        print(i)
-    if i%2 !=0:
-        print('printing odd number')
-        print(i)
-
+normal = "this is anoher test"
+number = int[input("add user input")]
+day2(normal, *number)
 
--- python_day2.py
+++ python_day2.py
@@ -1,6 +1,6 @@
 
 #Task1
-'''
+
 def day2(total, *args, **kargs):
     for i in range(0, len(args)):
         total = total + args[i]
@@ -11,6 +11,15 @@ number = number.split()
 for i in range(len(number)):
     number[i] = int(number[i])
 day2(total, *number)
-'''
+
 
 #Task2
+def task2(normal, *args, **kargs):
+     print(normal)
+     for key, value in kargs.items():
+         print(f'{key} {value}')
+normal = "This is for next task"
+mark = {"raul": "99", "avg_marks":"55", "failing_user": ['Hidan','Goku', 'Timon']}
+
+task2(normal, **mark)
+
